6f38d6f840bf1119781b84d768ac6cd3d1979992
Timo Reichl <timo.reichl@mailbox.org>
2022-06-02 22:08:09 +0200

    srcds: Improve server.sh with return values and helpful outputs
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- image/srcds/opt/server.sh
+++ image/srcds/opt/server.sh
@@ -5,6 +5,10 @@ if [ $(id -u) -ne ${STEAMCMD_UID} ]; then
 fi
 
 
+_is_running() {
+    return $(ps cax | grep ${1} > /dev/null)
+}
+
 
 healthy() {
     return $(tmux capture-pane -pt ${STEAMCMD_SERVER_SESSION_NAME} | grep -w "Connection to Steam servers successful." > /dev/null)
@@ -12,15 +16,23 @@ healthy() {
 
 
 wait() {
-    until ! _is_running srcds; do
-        :
-    done
-
+    until ! _is_running srcds; do :; done
     return 0
 }
 
 
+attach() {
+    tmux a -t ${STEAMCMD_SERVER_SESSION_NAME}
+}
+
+
 update() {
+    if _is_running steamcmd; then
+        return 1
+    fi
+
+    echo "Updating game server... Use 'server.sh attach' to attach to the SRCDS tmux session."
+
     tmux send-keys -t ${STEAMCMD_SERVER_SESSION_NAME} "steamcmd \
         +force_install_dir ${STEAMCMD_SERVER_HOME} \
         +login anonymous \
@@ -28,10 +40,22 @@ update() {
         +quit; tmux wait-for -S steamcmd-update-finished" "Enter"
 
     tmux wait-for steamcmd-update-finished
+    echo "Finished."
+
+    return 0
 }
 
+
 run() {
-    update
+    if _is_running steamcmd; then
+        echo "SteamCMD is currently updating. Use 'server.sh attach' to attach to the SRCDS tmux session."
+        return 1
+    fi
+
+    if _is_running srcds; then
+        echo "SRCDS is currently running. Use 'server.sh attach' to attach to the SRCDS tmux session."
+        return 2
+    fi
 
     steamcmd_cfg_dir=${STEAMCMD_SERVER_HOME}/${STEAMCMD_SERVER_GAME}/cfg
 
@@ -49,7 +73,9 @@ run() {
         echo "fps_max ${STEAMCMD_SERVER_FPSMAX}" >> ${steamcmd_cfg_dir}/tickrate_enabler.cfg
     fi
 
-    tmux send-keys -t ${STEAMCMD_SERVER_SESSION_NAME} "cd ${STEAMCMD_SERVER_HOME}; ./srcds_run \
+    echo "Starting SRCDS in tmux session..."
+
+    tmux send-keys -t ${STEAMCMD_SERVER_SESSION_NAME} "cd ${STEAMCMD_SERVER_HOME}; bash ./srcds_run \
         -console \
         -game ${STEAMCMD_SERVER_GAME} \
         +ip 0.0.0.0 \
@@ -59,14 +85,12 @@ run() {
         -tickrate ${STEAMCMD_SERVER_TICKRATE} \
         -threads ${STEAMCMD_SERVER_THREADS} \
         -nodev" "Enter"
+
+    until healthy; do : done
+
+    echo "Successfully started SRCDS in tmux session. Use 'server.sh attach' to attach to it."
+    return 0
 }
 
-attach() {
-    if ! ps cax | grep srcds > /dev/null; then
-        run
-    fi
-
-    tmux a -t ${STEAMCMD_SERVER_SESSION_NAME}
-}
 
 ${@}
