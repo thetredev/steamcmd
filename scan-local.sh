35a63f0c322a29f6926b35f85a1fc593b568eef2
Timo Reichl <timo.reichl@mailbox.org>
2022-06-18 14:46:50 +0200

    scan-local.sh: Update trivy image version to 0.29.0
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- scan-local.sh
+++ scan-local.sh
@@ -4,4 +4,4 @@ source ./local-vars.sh
 docker run --rm \
     -v ~/.trivy-cache:/root/.cache \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
-    aquasec/trivy:0.28.1-amd64 image --severity ${2:-CRITICAL,HIGH,MEDIUM,LOW} "${REGISTRY_IMAGE}:${1:-srcds}"
+    aquasec/trivy:0.29.0-amd64 image --severity ${2:-CRITICAL,HIGH,MEDIUM,LOW} "${REGISTRY_IMAGE}:${1:-srcds}"
