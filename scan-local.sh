9165593cde05fc15c01876517f1d99d70541d704
Timo Reichl <timo.reichl@mailbox.org>
2022-06-23 23:40:12 +0200

    Fix #10
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- scan-local.sh
+++ scan-local.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 source ./local-vars.sh
+
+git_root=$(git rev-parse --show-toplevel)
+
 docker run --rm \
+    -v ${git_root}/.trivyignore:/.trivyignore:ro \
     -v ~/.trivy-cache:/root/.cache \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
     aquasec/trivy:0.29.0-amd64 image --severity ${2:-CRITICAL,HIGH,MEDIUM,LOW} "${REGISTRY_IMAGE}:${1:-srcds}"
