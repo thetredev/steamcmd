bbf30faf2cfff2feee58b77de2b89172027b2f82
Timo Reichl <timo.reichl@mailbox.org>
2022-07-10 15:23:03 +0200

    scan-local.sh: Add dockle scan
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- scan-local.sh
+++ scan-local.sh
@@ -3,9 +3,14 @@
 source ./local-vars.sh
 
 git_root=$(git rev-parse --show-toplevel)
+docker_image="${REGISTRY_IMAGE}:${1:-srcds}"
 
 docker run --rm \
     -v ${git_root}/.trivyignore:/.trivyignore:ro \
     -v ~/.trivy-cache:/root/.cache \
     -v /var/run/docker.sock:/var/run/docker.sock:ro \
-    aquasec/trivy:0.29.2-amd64 image --severity ${2:-CRITICAL,HIGH,MEDIUM,LOW} "${REGISTRY_IMAGE}:${1:-srcds}"
+    aquasec/trivy:0.29.2-amd64 image --severity ${2:-CRITICAL,HIGH,MEDIUM,LOW} ${docker_image}
+
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock:ro \
+    goodwithtech/dockle:v0.4.5-amd64 ${docker_image}
