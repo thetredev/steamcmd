f038cd14b6d8318ac85af2dd15774394f1fe5e96
Timo Reichl <timo.reichl@mailbox.org>
2022-09-14 19:51:38 +0200

    Add SSHD for manipulating server files
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- image/games/base/docker-entrypoint.sh
+++ image/games/base/docker-entrypoint.sh
@@ -3,6 +3,17 @@
 # Set original entrypoint
 set -- tini -- start.sh ${@}
 
+# Helper function to fix host keys should they not exist
+prepare_ssh_host_key() {
+    host_key_type="${1}"
+    host_key_path="/opt/ssh/ssh_host_${host_key_type}_key"
+
+    if [[ ! -f ${host_key_path} ]]; then
+        rm -f "${host_key_path}*"
+        ssh-keygen -q -N "" -t ${host_key_type} -f ${host_key_path}
+    fi
+}
+
 # Fix file and directory permissions if run as root
 if [ $(id -u) -eq 0 ]; then
 
@@ -31,6 +42,31 @@ if [ $(id -u) -eq 0 ]; then
     echo "Fixing ownership of ${tmux_socket_dir}"
     mkdir -p ${tmux_socket_dir}
     chown -R steamcmd:steamcmd ${tmux_socket_dir}
+
+    if [[ "${STEAMCMD_SSH_SERVER_ENABLE}" == "1" ]]; then
+        # Prepare SSH server
+        echo "Preparing SSH server..."
+        mkdir -p "${STEAMCMD_USER_HOME}/.ssh"
+
+        echo -n "${STEAMCMD_SSH_AUTHORIZED_KEYS}" | base64 -d > "${STEAMCMD_USER_HOME}/.ssh/authorized_keys"
+
+        if [ $? -ne 0 ]; then
+            echo "${STEAMCMD_SSH_AUTHORIZED_KEYS}" > "${STEAMCMD_USER_HOME}/.ssh/authorized_keys"
+        fi
+
+        chown -R steamcmd:steamcmd "${STEAMCMD_USER_HOME}/.ssh"
+        chmod 0700 "${STEAMCMD_USER_HOME}/.ssh"
+        chmod 0600 "${STEAMCMD_USER_HOME}/.ssh/authorized_keys"
+
+        mkdir -p /opt/ssh
+        prepare_ssh_host_key "dsa"
+        prepare_ssh_host_key "rsa"
+        prepare_ssh_host_key "ecdsa"
+        prepare_ssh_host_key "ed25519"
+
+        # Run the server
+        /usr/sbin/sshd
+    fi
 
     # Call to gosu to drop from root user to steamcmd user
     # when running original entrypoint
