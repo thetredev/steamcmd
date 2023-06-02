d509303d0946e3093420c7b77925e1b7d0bbefbe
Timo Reichl <thetredev@gmail.com>
2023-06-03 00:55:59 +0200

    CI: Fix nightly base image check
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- scripts/compare_tags.py
+++ scripts/compare_tags.py
@@ -6,10 +6,6 @@ remote_tags = sys.argv[2].split("\n")
 current_tag_index = remote_tags.index(current_tag)
 remote_tags_of_interest = remote_tags[current_tag_index:]
 
-# -> couldn't find any newer tags
-if len(remote_tags_of_interest) < 2:
-    exit(1)
-
-# -> newer tag found, so print it
-print(remote_tags_of_interest[-1])
-exit(0)
+if len(remote_tags_of_interest) > 1:
+    # -> newer tag found, so print it
+    print(remote_tags_of_interest[-1])
